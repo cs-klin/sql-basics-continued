adfeee5cbf0f4185c9f38db6deecb5b8b2c904ba
Julia Fout <juuuulash@icloud.com>
2023-12-12 17:47:17 -0600

    num 15

--- sql-practice/phase-3.sql
+++ sql-practice/phase-3.sql
@@ -8,11 +8,20 @@ INSERT INTO coffee_orders(is_redeemed, ordered_at) VALUES (0, CURRENT_TIMESTAMP)
 INSERT INTO customers (name, email, phone) VALUES ("Monica", "monica@friends.show", 2222222222), ("Phoebe", "phoebe@friends.show", 3333333333);
 
 UPDATE customers SET points = points + 3 WHERE name = "Phoebe";
-INSERT INTO coffee_orders (is_redeemed, ordered_at) VALUES (0, CURRENT_TIMESTAMP);
+INSERT INTO coffee_orders DEFAULT VALUES;
+INSERT INTO coffee_orders DEFAULT VALUES;
+INSERT INTO coffee_orders DEFAULT VALUES;
 
 UPDATE customers SET points = points + 4 WHERE name = "Rachel";
 INSERT INTO coffee_orders (is_redeemed, ordered_at) VALUES (0, CURRENT_TIMESTAMP);
+INSERT INTO coffee_orders (is_redeemed, ordered_at) VALUES (0, CURRENT_TIMESTAMP);
+INSERT INTO coffee_orders (is_redeemed, ordered_at) VALUES (0, CURRENT_TIMESTAMP);
+INSERT INTO coffee_orders (is_redeemed, ordered_at) VALUES (0, CURRENT_TIMESTAMP);
+
 UPDATE customers SET points = points + 4 WHERE name = "Monica";
+INSERT INTO coffee_orders (is_redeemed, ordered_at) VALUES (0, CURRENT_TIMESTAMP);
+INSERT INTO coffee_orders (is_redeemed, ordered_at) VALUES (0, CURRENT_TIMESTAMP);
+INSERT INTO coffee_orders (is_redeemed, ordered_at) VALUES (0, CURRENT_TIMESTAMP);
 INSERT INTO coffee_orders (is_redeemed, ordered_at) VALUES (0, CURRENT_TIMESTAMP);
 
 SELECT points FROM customers WHERE name = "Monica";
@@ -28,17 +37,38 @@ INSERT INTO customers (name, email) VALUES ("Joey", "joey@friends.show"), ("Chan
 
 UPDATE customers SET points = points + 6 WHERE name = "Ross";
 INSERT INTO coffee_orders (is_redeemed, ordered_at) VALUES (0, CURRENT_TIMESTAMP);
+INSERT INTO coffee_orders (is_redeemed, ordered_at) VALUES (0, CURRENT_TIMESTAMP);
+INSERT INTO coffee_orders (is_redeemed, ordered_at) VALUES (0, CURRENT_TIMESTAMP);
+INSERT INTO coffee_orders (is_redeemed, ordered_at) VALUES (0, CURRENT_TIMESTAMP);
+INSERT INTO coffee_orders (is_redeemed, ordered_at) VALUES (0, CURRENT_TIMESTAMP);
+INSERT INTO coffee_orders (is_redeemed, ordered_at) VALUES (0, CURRENT_TIMESTAMP);
 
-
-
-
+--QUERY FOR THE IDs here: --hw for monday--select statement
+SELECT * FROM coffee_orders;
 
 
 UPDATE customers SET points = points + 3 WHERE name = "Monica";
-INSERT INTO coffee_orders (is_redeemed, ordered_at) VALUES (0, CURRENT_TIMESTAMP);
+INSERT INTO coffee_orders DEFAULT VALUES;
 
 SELECT points FROM customers WHERE name = "Phoebe";
 UPDATE customers SET points = points + 1 WHERE name = "Phoebe";
 INSERT INTO coffee_orders (is_redeemed, ordered_at) VALUES (0, CURRENT_TIMESTAMP);
 
-SELECT id FROM coffee_orders WHERE id = 6;
+
+DELETE FROM coffee_orders WHERE id=18;
+DELETE FROM coffee_orders WHERE id=19;
+-- manually count back his orders to find the id OR... do a query for all coffees
+-- delete coffee with the most recent two
+--DELETE HEREE
+--UPDATE HIS POINTS:
+UPDATE customers SET points = points - 2 WHERE name='Ross';
+
+--13:
+UPDATE customers SET points = points + 2 WHERE name='Joey';
+INSERT INTO coffee_orders DEFAULT VALUES;
+INSERT INTO coffee_orders DEFAULT VALUES;
+
+--14:
+SELECT points from customers WHERE name='Monica';
+INSERT INTO coffee_orders (is_redeemed) VALUES (TRUE);
+UPDATE customers SET points = points - 10 WHERE name='Monica';
