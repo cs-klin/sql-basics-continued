4cfa2e558c8c4585325b66b9e2f925d44ea214f4
Julia Fout <juuuulash@icloud.com>
2023-12-12 18:25:20 -0600

    started phase 4

--- sql-practice/phase-4.sql
+++ sql-practice/phase-4.sql
@@ -1,1 +1,26 @@
--- Your code here 
+-- Your code here
+
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS reviews;
+DROP TABLE IF EXISTS parties;
+
+CREATE TABLE employees(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name VARCHAR(40) NOT NULL,
+    last_name VARCHAR(40) NOT NULL,
+    department VARCHAR(100),
+    role VARCHAR(100),
+    dating_who VARCHAR(40)
+);
+
+CREATE TABLE reviews(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    who_for VARCHAR,
+    rating number(10,1)
+);
+
+CREATE TABLE parties(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    is_onsite BOOLEAN,
+    budget INTEGER
+);
